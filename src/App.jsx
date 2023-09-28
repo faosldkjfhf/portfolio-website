aa5ad8994f8babafbbf2d6d4e0460b426a3d391b
Kevin Liu <kliu0932@gmail.com>
2023-09-28 11:54:39 -0400

    trying another fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./home";
+import "./index.css";
 
 function App() {
     // const [count, setCount] = useState(0)
