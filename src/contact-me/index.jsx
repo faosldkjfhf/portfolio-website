72ed57bac2a5b4d3c26f98d3e211032419855b98
Kevin Liu <kliu0932@gmail.com>
2023-10-11 01:59:52 -0400

    small changes to contact card

--- src/contact-me/index.jsx
+++ src/contact-me/index.jsx
@@ -5,10 +5,26 @@ function ContactCard() {
         <div className="mx-auto align-middle items-center list-group-item">
             <div className="text-white font-bold text-4xl overflow-hiden">
                 <div>Contact Me</div>
+                <div className="text-sm text-slate-500 font-normal my-3">
+                    You can reach me through email at liu.kevi@northeastern.edu
+                    or through call at (703)388-6774. Feel free to reach out
+                    through any of the other links below as well!
+                </div>
                 <div className="text-xl">
-                    <Link to="https://www.linkedin.com/in/kliu0932">
-                        <i className="fa-brands fa-linkedin"></i> Kevin Liu
-                    </Link>
+                    <ul className="list-disc list-inside">
+                        <li className="list-item">
+                            <Link to="https://www.linkedin.com/in/kliu0932">
+                                <i className="fa-brands fa-linkedin"></i> Kevin
+                                Liu
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="https://www.github.com/faosldkjfhf">
+                                <i className="fa-brands fa-github"></i>{" "}
+                                faosldkjfhf
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
