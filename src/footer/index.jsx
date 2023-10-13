74ddfeb7243f7b477a5ad00700979d43926c70f2
Kevin Liu <kliu0932@gmail.com>
2023-10-13 14:35:48 -0400

    got rid of dark mode stuff

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow m-4 ml-7 dark:bg-gray-900 hover:bg-gray-900 hover:scale-110 transition-all">
+    <footer className="rounded-lg shadow m-4 ml-7 bg-gray-900  hover:scale-110 transition-all">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm sm:text-center dark:text-gray-400">
           This website is a work in progress.
         </span>
       </div>
--- src/nav-bar/index.jsx
+++ src/nav-bar/index.jsx
@@ -2,9 +2,14 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 sticky top-0 z-20 transition-all">
+    <nav className=" border-gray-200 bg-gray-900 sticky top-0 z-20 transition-all">
       <div className="flex flex-wrap items-center justify-between p-4">
-        <ul className="font-medium flex flex-row p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-slate-800 md:dark:bg-slate-900 dark:border-slate-700">
+        <ul
+          className="font-medium flex flex-row p-4 md:p-0 mt-4 border 
+                    rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0
+                     bg-slate-800 md:bg-slate-900
+                     border-slate-700"
+        >
           <li>
             <NavLink
               to="/"
